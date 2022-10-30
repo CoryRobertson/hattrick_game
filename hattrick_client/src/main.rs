1ec5171998bc94fe49eb2f033a499b0633c4a002
coryrobertson <cory.robertson17@gmail.com>
2022-10-29 17:34:10 -0700

    Cleaned up Cargo.toml, as well as added ping text, cleaned up code as well.

--- hattrick_client/src/main.rs
+++ hattrick_client/src/main.rs
@@ -1,34 +1,63 @@
 mod packets;
 
-use std::borrow::BorrowMut;
-use std::collections::HashMap;
 use std::io::{Read, Write};
 use std::net::TcpStream;
-use std::sync::{Arc, LockResult, Mutex};
+use std::sync::{Arc, Mutex};
 use std::thread;
-use std::thread::sleep;
+use std::thread::{JoinHandle, sleep};
 use std::time::{Duration, SystemTime};
 use macroquad::prelude::*;
-use once_cell::unsync::Lazy;
-
 use crate::packets::packets::{ClientState, GameState};
-
-
-
-// static mut STATIC_GAME_STATE: Lazy<GameState> = Lazy::new(|| {
-//     GameState{ time: SystemTime::UNIX_EPOCH, x: 0.0, y: 0.0, client_list: Default::default() }
-// });
 
 
 #[macroquad::main("???")]
 async fn main() {
     println!("I am the client");
 
-    let mut game_state = Arc::new(Mutex::new(GameState::default()));
 
+    let game_state = Arc::new(Mutex::new(GameState::default()));
+    let running_thread_state = Arc::new(Mutex::new(true));
+    let connect_thread = spawn_connect_thread(game_state.clone(),running_thread_state.clone());
 
-    let connect_game_state = Arc::clone(&game_state);
-    let connect_thread = thread::spawn(move || {
+    loop {
+        clear_background(WHITE);
+
+        // get the new game state that was most recently received from the connection thread
+        let local_gs = {
+          game_state.lock().unwrap().clone()
+        };
+
+        let ping = SystemTime::now().duration_since(local_gs.clone().time).unwrap();
+        let ping_color = {
+            if ping.as_millis() > 16 { RED }else { GREEN }
+        };
+        draw_text(&*format!("Ping: {:.2}ms", ping.as_secs_f64() * 1000.0), 10., 10., 18., ping_color);
+
+        // render each client from their client state
+        for client in local_gs.clone().client_list {
+            let client_state = client.1;
+            draw_circle(client_state.mouse_pos.0, client_state.mouse_pos.1, 15.0, RED);
+
+        }
+
+        draw_circle(local_gs.x as f32 + 200.0, local_gs.y as f32 + 200.0, 15.0, RED);
+
+        if is_key_pressed(KeyCode::Escape) {
+            let mut end = running_thread_state.lock().unwrap();
+            *end = false;
+            println!("disconnected from connection thread");
+            break;
+        }
+
+        frame_delay().await;
+        next_frame().await;
+    }
+
+    let _ = connect_thread.join();
+}
+
+fn spawn_connect_thread(game_state:Arc<Mutex<GameState>>, running: Arc<Mutex<bool>>) -> JoinHandle<()> {
+    thread::spawn(move || {
         let mut stream = TcpStream::connect("127.0.0.1:8111").unwrap();
         println!("connected");
         loop {
@@ -37,27 +66,29 @@ async fn main() {
             let client_packet = ClientState{ time: SystemTime::now(), mouse_pos: mouse_position() };
             let ser = serde_json::to_string(&client_packet).unwrap();
 
-            let _ = stream.read(&mut buf);
-            let _ = stream.write(ser.as_bytes());
-            let _ = stream.flush();
+            let read = stream.read(&mut buf);
+            let write = stream.write(ser.as_bytes());
+            let flush = stream.flush();
+
+            if read.is_err() || write.is_err() || flush.is_err() {
+                // gracefully close thread if internet connection
+                break;
+            }
+
             let mut cleaned_buf = vec![];
             for value in buf { // make small buffer of the data into a vector sent by the server
                 if !String::from_utf8_lossy(&[value]).contains("\0") {
                     cleaned_buf.push(value);
                 }
             }
-            let o = String::from_utf8(Vec::from(cleaned_buf)).unwrap();
-            match serde_json::from_str::<GameState>(&*o) {
-                Ok(gs) => {
-                    //println!("{:?}", gs.client_list);
+            let output_from_buf = String::from_utf8(Vec::from(cleaned_buf)).unwrap();
 
-                    //unsafe { *STATIC_GAME_STATE = gs.clone(); }
-                    match connect_game_state.lock().borrow_mut() {
-                        Ok(lock) => {
-                            lock.x = gs.x.clone();
-                            lock.y = gs.y.clone();
-                            lock.time = gs.time.clone();
-                            lock.client_list = gs.client_list.clone();
+            match serde_json::from_str::<GameState>(&*output_from_buf) {
+                Ok(gs) => {
+                    match game_state.lock() {
+                        Ok(mut lock) => {
+                            *lock = gs.clone();
+
                         }
                         Err(e) => {
                             println!("mutex guard error: {}",e);
@@ -66,51 +97,15 @@ async fn main() {
                 }
                 Err(e) => {println!("failed to parse: {}", e);}
             };
-            //println!("aquiring lock");
-            // let mut lock = connect_game_state.lock().unwrap();
-            // lock.time = deser.time;
 
-
-            // for client in deser.clone().client_list {
-            //     println!("{},{}",client.1.mouse_pos.0,client.1.mouse_pos.1);
-            // }
-            let _ = stream.flush();
-        }
-
-    });
-
-    let game_game_state = Arc::clone(&game_state);
-    loop {
-        {
-            let local_gs = {
-              game_game_state.lock().unwrap().clone()
-            };
-            // let local_gs = unsafe { STATIC_GAME_STATE.clone() };
-            clear_background(WHITE);
-
-            for client in local_gs.clone().client_list {
-                let client_state = client.1;
-                //println!("{} {}", client_state.mouse_pos.0, client_state.mouse_pos.1);
-                draw_circle(client_state.mouse_pos.0, client_state.mouse_pos.1, 15.0, RED);
-
+            if *running.lock().unwrap() == false {
+                // if the thread running state has been instructed to stop, then we break out of the loop gracefully
+                break;
             }
 
-            //println!("{:?}", mouse_position());
-
-            draw_circle(local_gs.x as f32 + 200.0, local_gs.y as f32 + 200.0, 15.0, RED);
-
-            draw_text(format!("Server Time: {}", local_gs).as_str(), 50., 50., 12., BLACK);
         }
-
-        if is_key_pressed(KeyCode::Escape) {
-            break;
-        }
-        // sleep(Duration::from_millis(500));
-        frame_delay().await;
-        next_frame().await;
-
-    }
-    let _ = connect_thread.join();
+        println!("connection thread finished");
+    })
 }
 
 async fn frame_delay() {
