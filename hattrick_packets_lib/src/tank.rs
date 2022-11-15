82037326009a4f4c480a21037d7867f2f4b90f97
coryrobertson <cory.robertson17@gmail.com>
2022-11-15 00:30:55 -0800

    Added TODO to tank.rs

--- hattrick_packets_lib/src/tank.rs
+++ hattrick_packets_lib/src/tank.rs
@@ -7,6 +7,11 @@ pub static TANK_TURN_SPEED: f32 = 2.0;
 pub static TANK_WIDTH: f32 = 20.0;
 pub static TANK_HEIGHT: f32 = 20.0;
 
+//TODO: create a tank bullet struct, has an x, y, xvel, yvel, and maybe a reference to its owner? or just the owners uuid.
+//  The bullet hits a tank, and the tank will be reset to a random position? unsure. The bullet has a function called check collide, which takes in the client list,
+//  and checks its point distance from each tank on the map on each frame to find if it is to hit a tank.
+//  It also has a function called step, which moves its location based on its xvel and yvel.
+
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct TankGameState {
     pub red_score: i32,
